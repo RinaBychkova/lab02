9bef697b45b0f7a362a9baae2070bb35add53954
RinaBychkova <SatiraGaret@yandex.ru>
2026-05-19 09:17:27 +0300

    fixed code style in patch1

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
-using namespace std;
-
+#include <string>
 int main() {
-    cout << "Hello world" << endl;
+    std::string name;
+    std::cout << "Enter your name: ";
+    std::cin >> name;
+    std::cout << "Hello world from @" << name << std::endl;
     return 0;
 }

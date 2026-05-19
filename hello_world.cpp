93a3a11e34fc57d83ad52417bf9c531a4c51c87c
RinaBychkova <SatiraGaret@yandex.ru>
2026-05-19 11:52:24 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,6 +6,6 @@ int main() {
     std::string name;
     std::cout << "Enter your name: ";
     std::cin >> name;
-    std::cout << "Hello world from @" << name << std::endl;
+    std::cout << "Hello, world from @" << name << std::endl;
     return 0;
 }
